398770f57f0d8b942e1b7af0a51df74b620dc2fb
AimBotYaster <yaser.tara22@gmail.com>
2018-04-18 09:15:20 +0200

    hello world erweitert

--- hello-world.py
+++ hello-world.py
@@ -4,6 +4,13 @@ meinname = input ("Wie ist dein Name")
 print("schön dich zu treffen",meinname)
 print ("Dein Name ist", (len(meinname)),"Zeichen lang")
 alter = input("wie alt bist du?")
-print("Du wirst in einem Jahr ",  int (alter) +1, "alt sein")
+alter = int(alter)
+if(alter > 100):
+    print ("Das ist wohl ein Scherz - oder? xD")
+    print("test")
+else:   
+    print("Du wirst in einem Jahr ", alter +1, "alt sein")
+    print("tschüss..")
+
 
 
